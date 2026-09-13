e2ec22ed688d087a11d062e14590f9d5898e58df
Johan-Castro537 <jstebancastro@estudiante.uniajc.edu.co>
2026-09-13 01:55:05 +0000

    acomodar codigo y errores

--- Actividad #1 Arreglos/Punto11.java
+++ Actividad #1 Arreglos/Punto11.java
@@ -1,42 +1,38 @@
 public class Punto11 {
 
-    int[] a = {3,7,3,-7,-2,5,1,-9};
+    public static void main(String[] args) {
+        int[] a = {3, 7, 3, -7, -2, 5, 1, -9};
 
-    int[] negativos = new int[a.length];
-    int[] positivos = new int[a.length];
-    int[] ceros = new int[a.length];
+        int[] negativos = new int[a.length];
+        int[] positivos = new int[a.length];
+        int[] ceros = new int[a.length];
 
-    int n = 0;
-    int p = 0;
-    int c = 0;
+        int n = 0;
+        int p = 0;
+        int c = 0;
 
-    for(int i = 0; i < a.length; i++){
-
-        if(a[i] < 0){
-            negativos[n] = a[i];
-            n++;
-
-        }else if(a[i] > 0){
-            positivos[p] = a[i];
-            p++;
-
-        }else{
-            ceros[c] = a[i];
-            c++;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < 0) {
+                negativos[n++] = a[i];
+            } else if (a[i] > 0) {
+                positivos[p++] = a[i];
+            } else {
+                ceros[c++] = a[i];
+            }
         }
-    }
 
         System.out.println("Negativos: ");
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             System.out.println(negativos[i]);
         }
         System.out.println("Positivos: ");
-        for(int i = 0; i < p; i++){
+        for (int i = 0; i < p; i++) {
             System.out.println(positivos[i]);
         }
 
         System.out.println("Ceros: ");
-        for(int i = 0; i < c; i++){ 
+        for (int i = 0; i < c; i++) {
             System.out.println(ceros[i]);
         }
     }
+}
