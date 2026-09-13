79997ad869e14a5b9482d727e12918420cb0ef9b
Johan-Castro537 <jstebancastro@estudiante.uniajc.edu.co>
2026-09-13 01:42:47 +0000

    declaro variables y recorro arreglo

--- Actividad #1 Arreglos/Punto11.java
+++ Actividad #1 Arreglos/Punto11.java
@@ -1,10 +1,17 @@
 public class Punto11 {
-    
+
     int[] a = {3,7,3,-7,-2,5,1,-9};
 
     int[] negativos = new int[a.length];
     int[] positivos = new int[a.length];
     int[] ceros = new int[a.length];
 
-    
+    int n = 0;
+    int p = 0;
+    int c = 0;
+
+    for(int i = 0; i < a.length; i++){
+
+        
+    }
 }
