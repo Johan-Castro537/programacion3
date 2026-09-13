30f334a829cef9f6d1552750737b4bc9fc59af00
Johan-Castro537 <jstebancastro@estudiante.uniajc.edu.co>
2026-09-13 15:37:13 +0000

    buscar numero y comparar con x

--- Arreglos #1/Arreglos.java
+++ Arreglos #1/Arreglos.java
@@ -106,6 +106,11 @@ public class Arreglos {
 
                 for (int i = 0; i < a.length; i++){
                     System.out.println("a[" + i + "] = " + a[i]);
+                    
+                for (int i = 0; i < a.length; i++) {
+                    if (a[i] == x) {
+                        contador++;
+                    }
                 }
             }
 
