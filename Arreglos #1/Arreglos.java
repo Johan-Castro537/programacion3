b7815f17e4c7044b02f396e891c6028a129be633
Johan-Castro537 <jstebancastro@estudiante.uniajc.edu.co>
2026-09-13 14:51:42 +0000

    declaración de los arreglos y variables

---
+++ Arreglos #1/Arreglos.java
@@ -0,0 +1,12 @@
+public class Arreglos {
+    public static void main(String[] args){
+        //PUNTO 7
+
+        int[] pares = new int[10];
+        int[] impares = new int[10];
+
+        int p = 0;
+        int im = 0;
+    }
+    
+}
