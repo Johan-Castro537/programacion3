6d73b9f1fc1258401cb1b76312d3e87ce697da25
Johan-Castro537 <jstebancastro@estudiante.uniajc.edu.co>
2026-09-13 00:33:40 +0000

    recorre el arreglo e impresion

--- Actividad #1 Arreglos/Punto9.java
+++ Actividad #1 Arreglos/Punto9.java
@@ -13,4 +13,15 @@ public class Punto9 {
         for (int i = 0; i < arreglo.length; i++) {
             arreglo[i] = scanner.nextInt();
         }
-}
+
+        System.out.println("Ingrese un número para buscar en el arreglo:");
+        numero = scanner.nextInt();
+
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] == numero) {
+                contador++;
+            }
+        }
+        System.out.println("El número " + numero + " aparece " + contador + " veces en el arreglo");
+    }  
+}     
