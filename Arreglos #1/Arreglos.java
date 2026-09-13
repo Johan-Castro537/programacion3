6b49fc65e92161adcd7ed1f83d8e26253e2eba98
Johan-Castro537 <jstebancastro@estudiante.uniajc.edu.co>
2026-09-13 14:57:13 +0000

    mostrar los numeros pares e impares y acomodar codigo

--- Arreglos #1/Arreglos.java
+++ Arreglos #1/Arreglos.java
@@ -1,6 +1,6 @@
 public class Arreglos {
-    public static void main(String[] args){
-        //PUNTO 7
+    public static void main(String[] args) {
+        // PUNTO 7
 
         int[] pares = new int[10];
         int[] impares = new int[10];
@@ -8,20 +8,27 @@ public class Arreglos {
         int p = 0;
         int im = 0;
 
-        for(int i = 0; i < 20;i++){
-            
-            int numero = (int)(Math.random()* (100 - 1 + 1) + 1);
+        for (int i = 0; i < 20; i++) {
 
-            if(numero % 2 == 0 && p < pares.length){
+            int numero = (int) (Math.random() * (100 - 1 + 1) + 1);
+
+            if (numero % 2 == 0 && p < pares.length) {
                 pares[p] = numero;
                 p++;
             }
-            if(numero % 2 != 0 && im < impares.length){
+            if (numero % 2 != 0 && im < impares.length) {
                 impares[im] = numero;
                 im++;
             }
 
         }
+        System.out.println("Números pares:");
+        for (int i = 0; i < p; i++) {
+            System.out.print(pares[i] + " ");
+        }
+        System.out.println("\nNúmeros impares:");
+        for (int i = 0; i < im; i++) {
+            System.out.print(impares[i] + " ");
+        }
     }
-    
 }
