c15813260ce4349e4e303b3f834d4fa8ca1f6752
Johan-Castro537 <jstebancastro@estudiante.uniajc.edu.co>
2026-09-13 15:14:06 +0000

    Menu y acomodo de puntos

--- Arreglos #1/Arreglos.java
+++ Arreglos #1/Arreglos.java
@@ -1,34 +1,58 @@
+import java.util.Scanner;
+
 public class Arreglos {
     public static void main(String[] args) {
-        // PUNTO 7
 
-        int[] pares = new int[10];
-        int[] impares = new int[10];
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
 
-        int p = 0;
-        int im = 0;
+        // Menu para acceder a los puntos del ejercicio
 
-        for (int i = 0; i < 20; i++) {
+        System.out.println("1. Punto 7");
+        System.out.println("2. Punto 8");
+        System.out.println("3. Punto 9");
+        System.out.println("4. Punto 10");
+        System.out.println("5. Punto 11");
+        System.out.println("6. Punto 12");
+        System.out.println("7. Punto 13");
+        System.out.println("0. Salir");
 
-            int numero = (int) (Math.random() * (100 - 1 + 1) + 1);
+        System.out.print("Ingrese el número del punto que quiere ejecutar: ");
+        opcion = scanner.nextInt();
 
-            if (numero % 2 == 0 && p < pares.length) {
-                pares[p] = numero;
-                p++;
-            }
-            if (numero % 2 != 0 && im < impares.length) {
-                impares[im] = numero;
-                im++;
-            }
+        switch (opcion) {
 
-        }
-        System.out.println("Números pares:");
-        for (int i = 0; i < p; i++) {
-            System.out.print(pares[i] + " ");
-        }
-        System.out.println("\nNúmeros impares:");
-        for (int i = 0; i < im; i++) {
-            System.out.print(impares[i] + " ");
+            // Punto 7
+
+            case 1:
+                int[] pares = new int[10];
+                int[] impares = new int[10];
+
+                int p = 0;
+                int im = 0;
+
+                for (int i = 0; i < 20; i++) {
+
+                    int numero = (int) (Math.random() * (100 - 1 + 1) + 1);
+
+                    if (numero % 2 == 0 && p < pares.length) {
+                        pares[p] = numero;
+                        p++;
+                    }
+                    if (numero % 2 != 0 && im < impares.length) {
+                        impares[im] = numero;
+                        im++;
+                    }
+
+                }
+                System.out.println("Números pares:");
+                for (int i = 0; i < p; i++) {
+                    System.out.print(pares[i] + " ");
+                }
+                System.out.println("\nNúmeros impares:");
+                for (int i = 0; i < im; i++) {
+                    System.out.print(impares[i] + " ");
+                }
         }
     }
 
@@ -39,5 +63,5 @@ public class Arreglos {
     int[] menor = 0;
     int[] vecesmayor = 0;
     int[] vecesmenor = 0;
-    
+
 }
