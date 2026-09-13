df72de975d4f22ffb7462f22042a2f3347fed15f
Johan-Castro537 <jstebancastro@estudiante.uniajc.edu.co>
2026-09-13 01:02:55 +0000

    recorre arreglo y sumar elementos opuestos

--- Actividad #1 Arreglos/Punto10.java
+++ Actividad #1 Arreglos/Punto10.java
@@ -2,6 +2,10 @@ public class Punto10 {
     public static void main(String[] args){
         int[] a = {1, 2, 3, 4, 5};
         int[] b = new int[4];
-        
+
+        for(int i = 0; i < b.length; i++){
+            b[i] = a[i] + a[a.length - 1 - i];
+        }
     }
+    
 }
