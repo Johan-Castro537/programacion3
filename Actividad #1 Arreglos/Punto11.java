d6f28632c1143f50721027913d4bff1b67f90424
Johan-Castro537 <jstebancastro@estudiante.uniajc.edu.co>
2026-09-13 01:41:30 +0000

    declaro arreglo a y n,p,c

---
+++ Actividad #1 Arreglos/Punto11.java
@@ -0,0 +1,10 @@
+public class Punto11 {
+    
+    int[] a = {3,7,3,-7,-2,5,1,-9};
+
+    int[] negativos = new int[a.length];
+    int[] positivos = new int[a.length];
+    int[] ceros = new int[a.length];
+
+    
+}
