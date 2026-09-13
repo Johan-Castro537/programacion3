0ab48a6dc351678174f006bcfcd3533a2c3e201b
Johan-Castro537 <jstebancastro@estudiante.uniajc.edu.co>
2026-09-13 00:02:51 +0000

    arreglo, variable, contador y recorrer arreglo

--- Actividad #1 Arreglos/Punto9.java
+++ Actividad #1 Arreglos/Punto9.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 
 public class Punto9 {
     public static void main(String[] args) {
-        scanner scanner = new Scanner(System.in);
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arreglo = new int[5];
+        int numero;
+        int contador = 0;
+
+        System.out.println("Ingrese los elementos del arreglo:");
+
+        for (int i = 0; i < arreglo.length; i++) {
+            arreglo[i] = scanner.nextInt();
+        }
 }
