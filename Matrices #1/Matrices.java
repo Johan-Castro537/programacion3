a2f12791417307ab8f736da9160e02f1f7c041e2
Johan-Castro537 <jstebancastro@estudiante.uniajc.edu.co>
2026-09-14 00:30:08 +0000

    Crear menu para acceder a cada ejercicio

---
+++ Matrices #1/Matrices.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Matrices {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int opcion;
+
+        System.out.println("1. Punto 17");
+        System.out.println("2. Punto 18");
+        System.out.println("3. Punto 19");
+        System.out.println("4. Punto 20");
+        System.out.println("5. Punto 21");
+        System.out.println("6. Punto 22");
+        System.out.println("7. Punto 23");
+        System.out.println("8. Punto 24");
+
+        System.out.print("Ingrese el número del punto que quiere ejecutar: ");
+        opcion = scanner.nextInt();
+
+        switch (opcion) {
+            case 1:
+                // Punto 17
+                
+        
+        }
+    }
+}
